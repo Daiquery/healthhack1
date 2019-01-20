d55f6f750ecbd4b9cb6b2d8e43f7a3db424dd66c
Nicholas <nicholas@Dontes-MacBook-Pro.local>
2019-01-20 08:29:32 -0800

    Initial Commit

---
+++ HH1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HH1
+//
+//  Created by Nicholas on 1/20/19.
+//  Copyright © 2019 Digital Art Crew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
